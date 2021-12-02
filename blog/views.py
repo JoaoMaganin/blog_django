f22a5cff3879e327ab17f3969a0aabdcd5294cd5
João Maganin <joaovitormontemor138@gmail.com>
2021-12-02 08:28:39 -0300

    Projeto Blog Template 1

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from blog.models import Post
+
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+
+def post(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'post.html', {'post': post})
